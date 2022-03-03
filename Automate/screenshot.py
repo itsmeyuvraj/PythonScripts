c89d6d5cf58a8c3638418e018019d1e280d9d9c1
Yuvraj Sharma <99718182+yuvrajpoudyal@users.noreply.github.com>
2022-03-03 15:35:41 +0530

    Delete Automate directory

--- Automate/screenshot.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-def capture():
-        random = time.time()
-        img = pyautogui.screenshot()
-        img.save("C:\\Users\\Yuvraj Sharma\\Pictures\\" + str(random) + ".jpg")
