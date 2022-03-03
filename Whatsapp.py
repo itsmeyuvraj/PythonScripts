9a0b9c97d887c7c0f4d06b29b44654816d9a20b9
Yuvraj Sharma <99718182+yuvrajpoudyal@users.noreply.github.com>
2022-03-03 22:46:09 +0530

    Update Whatsapp.py
    Removed number for privacy

--- Whatsapp.py
+++ Whatsapp.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg('+918902759196', 'This message was sent using python', 5,50)
+pywhatkit.sendwhatmsg('+91XXXXXXXXXXX', 'This message was sent using python', 5,50)
